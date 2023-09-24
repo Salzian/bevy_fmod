eba17bcc2f3407bca80224b3101b2f02e5958009
GitGhillie <jillisnoordhoek@gmail.com>
2023-09-24 22:27:24 +0200

    Add support for FMOD local parameters (#32)
    Exposes event_instance and adds an example

--- src/components/audio_source.rs
+++ src/components/audio_source.rs
@@ -9,7 +9,7 @@ use crate::components::velocity::Velocity;
 
 #[derive(Component)]
 pub struct AudioSource {
-    event_instance: EventInstance,
+    pub event_instance: EventInstance,
 }
 
 impl AudioSource {
