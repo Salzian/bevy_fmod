ba6340dd0a69e853026f07930ee0a30579e34a56
GitGhillie <jillisnoordhoek@gmail.com>
2023-10-22 10:25:51 +0200

    Provide bundles for spatial audio (#56)

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,7 @@
 pub use crate::components::audio_listener::AudioListener;
 pub use crate::components::audio_source::AudioSource;
+pub use crate::components::bundles::SpatialAudioBundle;
+pub use crate::components::bundles::SpatialListenerBundle;
 pub use crate::components::velocity::Velocity;
 pub use crate::fmod_plugin::FmodPlugin;
 pub use crate::fmod_studio::FmodStudio;
