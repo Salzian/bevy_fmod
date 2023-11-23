97841c667ba242a547d197b1c9487a2e6367dd24
GitGhillie <jillisnoordhoek@gmail.com>
2023-11-23 21:28:20 +0100

    More stable doppler

--- src/components/velocity.rs
+++ src/components/velocity.rs
@@ -1,4 +1,4 @@
-use bevy::app::{App, FixedUpdate, Plugin};
+use bevy::app::{App, Plugin, Update};
 use bevy::math::Vec3;
 use bevy::prelude::{Component, GlobalTransform, Query, Res, Time};
 
@@ -20,14 +20,17 @@ impl VelocityPlugin {
         velocity.iter_mut().for_each(|(mut velocity, transform)| {
             let current_position = transform.translation();
             let delta_position = current_position - velocity.last_position;
-            velocity.current_velocity = delta_position / time.delta().as_secs_f32();
-            velocity.last_position = current_position;
+
+            if time.delta().as_secs_f32() != 0.0 {
+                velocity.current_velocity = delta_position / time.delta().as_secs_f32();
+                velocity.last_position = current_position;
+            }
         })
     }
 }
 
 impl Plugin for VelocityPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(FixedUpdate, Self::update_velocity);
+        app.add_systems(Update, Self::update_velocity);
     }
 }
