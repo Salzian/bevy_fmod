c2552bb95dd08da71a2751c3aa295ddda139c424
GitGhillie <jillisnoordhoek@gmail.com>
2025-04-05 19:31:08 +0200

    Update rust edition, fix clippy

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
-/// This build script is needed to run the examples on Linux.
-/// Feel free to copy this to your own Bevy project.
-///
-/// Be aware that on Linux you also need to set LD_LIBRARY_PATH to include the libraries.
-/// There are a few ways to do it:
-///
-/// If you are working with an IDE it's easiest to add this to the run configuration.
-/// For example, in Rust Rover you set `Environment variables` to `LD_LIBRARY_PATH=./fmod/api/core/lib/x86_64:./fmod/api/studio/lib/x86_64`.
-///
-/// If you are running your executable directly (no IDE, no cargo) see https://www.hpc.dtu.dk/?page_id=1180
+//! This build script is needed to run the examples on Linux.
+//! Feel free to copy this to your own Bevy project.
+//!
+//! Be aware that on Linux you also need to set LD_LIBRARY_PATH to include the libraries.
+//! There are a few ways to do it:
+//!
+//! If you are working with an IDE it's easiest to add this to the run configuration.
+//! For example, in Rust Rover you set `Environment variables` to `LD_LIBRARY_PATH=./fmod/api/core/lib/x86_64:./fmod/api/studio/lib/x86_64`.
+//!
+//! If you are running your executable directly (no IDE, no cargo) see https://www.hpc.dtu.dk/?page_id=1180
 
 fn main() {
     #[cfg(target_os = "linux")]
